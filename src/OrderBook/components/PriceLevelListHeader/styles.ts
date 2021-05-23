4140d1a2142513ca9774d2dc443f541a420efe39
Leonardo Cristofani <leo.cristofani@gmail.com>
2021-05-22 22:00:11 -0300

    hide asks header in small devices

--- src/OrderBook/components/PriceLevelListHeader/styles.ts
+++ src/OrderBook/components/PriceLevelListHeader/styles.ts
@@ -6,6 +6,9 @@ export interface StyleProps {
 
 const useStyles = makeStyles<Theme, StyleProps>((theme) => ({
   root: {
+    [theme.breakpoints.down("xs")]: {
+      display: ({ isBid }) => (isBid ? "flex" : "none"),
+    },
     [theme.breakpoints.up("sm")]: {
       flexDirection: ({ isBid }) => (isBid ? "row" : "row-reverse"),
     },
